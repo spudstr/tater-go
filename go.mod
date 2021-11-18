32e706adaad169877b499cd24c9229a3efd6c661
Zak Thompson <zthompson@securityscorecard.io>
2021-11-18 15:55:13 -0500

    its alive

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/spudstr/tater-go
 go 1.17
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
-	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20211117155847-120650a500bb // indirect
-	google.golang.org/grpc v1.42.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/grpc v1.42.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,91 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"log"
 	"os"
+
+	pb "github.com/spudstr/tater-go/schema"
+	"google.golang.org/grpc"
 )
+
+type grc_details struct {
+	domain   string
+	username string
+	password string
+}
 
 func main() {
 	domain := os.Getenv("quodd_domain")
 	username := os.Getenv("quodd_username")
 	password := os.Getenv("quodd_password")
 	gateway := os.Getenv("quodd_gateway")
+	server := "gateway.quodd.com:55012"
 
 	if domain == "" || username == "" || password == "" || gateway == "" {
 		panic("Missing environment variables")
 	}
+	// open grpc connection to quodd
+	// dial server
+	conn, err := grpc.Dial(server, grpc.WithInsecure())
+	defer conn.Close()
+
+	if err != nil {
+		log.Fatalf("can not connect with server %v", err)
+	}
+
+	// populate Authrequest
+	var authReq pb.AuthRequest
+	authReq.Username = []byte(username)
+	authReq.Password = []byte(password)
+	authReq.Domain = []byte(domain)
+
+	stub := pb.NewTableApiClient(conn)
+
+	// call auth
+	authResp, err := stub.Authenticate(context.Background(), &authReq)
+	if err != nil {
+		log.Fatalf("can not auth with server %v", err)
+	}
+	fmt.Printf("%+v\n", authResp)
+
+	// call get
+	var streamRequest pb.StreamRequest
+	var keys [][]byte
+	keys = append(keys, []byte("O:TSLA*"))
+	keys = append(keys, []byte("O:SPY*"))
+
+	streamRequest.SessionToken = authResp.SessionToken
+	streamRequest.Keys = keys
+
+	stream, err := stub.StreamData(context.Background(), &streamRequest)
+	if err != nil {
+		log.Fatalf("can not get with server %v", err)
+	}
+
+	done := make(chan bool)
+	counter := int64(0)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				done <- true //close(done)
+				return
+			}
+			if err != nil {
+				log.Fatalf("can not receive %v", err)
+			}
+			fmt.Printf("%+v\n", counter)
+			counter++
+			log.Printf("1")
+			_ = resp
+		}
+	}()
+
+	<-done
+	log.Printf("Finished")
 
 }
